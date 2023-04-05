0ccd65cde67f09bdd54920afb09ef5c4ddc5c10e
GeorgiMaRkOv9 <georgi.markov9@googlemail.com>
2023-04-05 22:14:45 +0200

    Add work page and workItemArray

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
 
 const app = express();
 
 let itemArray = ["Buy Food", "Cook Food", "Eat Food"];
+
+let workItemArray = [];
 
 app.set("view engine", "ejs");
 
@@ -24,19 +27,33 @@ app.get("/", function(req, res) {
  let day = today.toLocaleDateString("en-US", options);
 
 
-    res.render("list", {kindOfDay: day, newListItemArray: itemArray});
+ res.render("list", {listTitle: day, newListItemArray: itemArray});
+
 });
 
 app.post("/", function(req, res) {
-  let item = req.body.newItem;
 
-   itemArray.push(item);
+   let item = req.body.newItem;
 
-   res.redirect("/");
+   if (req.body.list === "Work") {
+      workItemArray.push(item);
+      res.redirect("/work");
+   } else {
+      itemArray.push(item);
+      res.redirect("/");
+   };
 
+  
 });
+
+app.get("/work", function(req, res) {
+   res.render("list", {listTitle: "Work List", newListItemArray: workItemArray});
+});
+
+
 
 app.listen(3000, () =>{
     console.log("Server started on port 3000!");
 });
+
 
